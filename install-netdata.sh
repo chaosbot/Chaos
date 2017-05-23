3427f80c0596f9377661f5f380a4aebdc5f58f82
Daniil Gentili <daniil.gentili.dg@gmail.com>
2017-05-23 16:08:52 +0100

    Do not reinstall netdata if it is already installed

--- install-netdata.sh
+++ install-netdata.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd /tmp/
+[ -d netdata ] && exit
 
 curl -Ss 'https://raw.githubusercontent.com/firehol/netdata-demo-site/master/install-required-packages.sh' >/tmp/kickstart.sh && bash /tmp/kickstart.sh -i netdata-all --dont-wait
 
-rm -rf netdata
 git clone https://github.com/firehol/netdata.git --depth=1
 cd netdata
 
