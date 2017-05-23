cadb0372935ac0cdc17d80edceebfdfb0813d0b6
Daniele <oppifjellet@gmail.com>
2017-05-23 21:42:01 +0000

    cleanup

--- server/server.py
+++ server/server.py
@@ -1,19 +1,6 @@
 import http.server
 import socketserver
 
-import os
-import random
-
-from flask import Flask, Markup, render_template
-app = Flask(__name__, template_folder='.')
-
-@app.route("/")
-def server_random_content():
-    stuff = os.listdir('content')
-    chosen_one = random.choice(stuff)
-    with open(os.path.join('content', chosen_one), 'r') as fp:
-        return render_template('index.html', content=Markup(fp.read()))
-
 #set the process name to "chaos_server" so we can easily kill it with "pkill chaos_server"
 def set_proc_name(newname):
     from ctypes import cdll, byref, create_string_buffer
@@ -23,8 +10,8 @@ def set_proc_name(newname):
     libc.prctl(15, byref(buff), 0, 0, 0)
 set_proc_name("chaos_server")
 
-# initialize random seed
-random.seed()
-
 #start server on port 80
-app.run(host="0.0.0.0", port=80, threaded=False)
+PORT = 80
+Handler = http.server.SimpleHTTPRequestHandler
+httpd = socketserver.TCPServer(("", PORT), Handler)
+httpd.serve_forever()
