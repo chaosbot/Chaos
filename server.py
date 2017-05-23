9e3d69963780efa42cea0b5a5cd8964197d6c3a8
Richard <richard.anaya@gmail.com>
2017-05-23 00:26:08 -0500

    Create server.py

---
+++ server.py
@@ -0,0 +1,7 @@
+#create a simple http server
+import http.server
+import socketserver
+PORT = 80
+Handler = http.server.SimpleHTTPRequestHandler
+httpd = socketserver.TCPServer(("", PORT), Handler)
+httpd.serve_forever()
