64eba09fd35b94ff09d496d49e3a3d45dc071667
Ameliorate <Ameliorate@users.noreply.github.com>
2017-05-22 21:41:35 -0700

    Use exec instead of while in start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 
-while true ; do
-    python3 chaos.py
-    echo "ChaosBot died, restarting in 30 seconds..."
-    sleep 30
-    echo "Restarting ChaosBot..."
-done
+python3 chaos.py
+echo "ChaosBot died, restarting in 30 seconds..."
+sleep 30
+echo "Restarting ChaosBot..."
+
+exec "$ScriptLoc" 
+# This is done with a exec instead of a while loop so that the script will be reloaded if it is modified.
