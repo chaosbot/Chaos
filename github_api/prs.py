0e2861751f813aef1acbb3427ab2fc1ae68256da
MRHwick <MatthewRHardwick@gmail.com>
2017-05-24 11:56:29 -0400

    Make case insensitive

--- github_api/prs.py
+++ github_api/prs.py
@@ -115,7 +115,7 @@ def owner_name_added_in_diff(api, urn, pr_num, owner_name):
     path = 'http://github.com/{urn}/pull/{pr_num}.diff'
     diff = api('get', path)
     regex = '\+.+{username}'.format(username=owner_name)
-    return re.match(regex, diff)
+    return re.match(regex, diff, re.IGNORECASE)
 
 
 def get_ready_prs(api, urn, window):
