f4cb1cb34d7d878b34bffe1442b0f5518c347057
Micah Nordland <micah@rehack.me>
2017-05-23 22:12:49 -0400

    Basic responder reddit bot

--- redditchaosbot.py
+++ redditchaosbot.py
@@ -1,35 +1,29 @@
+import logging
 import praw
 from praw.models import MoreComments
-import pprint
 
 
 '''Authenticated instance of Reddit'''
 # --------------------------------------------------------------------
 # https://praw.readthedocs.io/en/latest/getting_started/installation.html
 # https://www.reddit.com/prefs/apps
-reddit = praw.Reddit(user_agent='Chaosbot social experiment',
-                     client_id='KbKcHMguzNRKUw', client_secret="n-2lxQYOKtJinlJUWJr_Zv7g1rw",
-                     username='redditchaosbot', password='cha0sb0t')
+reddit = praw.Reddit(
+    user_agent='Chaosbot social experiment',
+    client_id='WbDSRDEenyFRYw',
+    client_secret="CzAcNHiH91NM6baL1ypzQnkyFG8",
+    username='chaosthebotreborn',
+     password='9ws#?&+@3YBHLB~iVH,;5:rRr]@<,tW]J(n]t#`dZ;a7ZbsqJVny)(2yUO3d5d}VjW=L.U7aS4*E<-6x&WE$qa6>#{\Z\d]N-}T7')
 
-reddit.read_only = True
-
-submission = reddit.submission(id='6criij')
-subreddit = reddit.subreddit('programming')
-# --------------------------------------------------------------------
-
-
-'''WHAT ARE PEOPLE SAYING ABOUT CHAOSBOT?'''
-# --------------------------------------------------------------------
-# in reddit thread: https://goo.gl/5ETNmF
-submission.comment_sort = 'new'
-top_level_comments = list(submission.comments)
-newcom = top_level_comments[1]
-print(newcom.body)
+subreddit = reddit.subreddit('chaosthebot')
+log = logging.getLogger("chaosbot")
 
 
+def process_submission(submission):
+    log.info("Processing", submission.title)
+    for comment in submission.comments:
+        if "hey chaosbot" in comment.body.lower():
+            comment.reply("Hey {}!".format(comment.author.name))
 
-'''FIND COMMENTS ABOUT CHAOS'''
-# --------------------------------------------------------------------
-# TBD
-# for comment in reddit.subreddit('programming').comments(limit=5):
-# import pdb; pdb.set_trace()
+
+for submission in subreddit.stream.submissions():
+    process_submission(submission)
