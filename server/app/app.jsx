40413be0473f497060e44632efde40d626fa64a7
Nikola Puric <nikola.puric92@gmail.com>
2017-05-26 01:41:35 +0200

    Removed ugly text.

--- server/app/app.jsx
+++ server/app/app.jsx
@@ -3,7 +3,7 @@ import { h, Component } from 'preact';
 export default class App extends Component {
     render() {
         return (
-            <div>App goes here...</div>
+            <div></div>
         );
     }
 }
