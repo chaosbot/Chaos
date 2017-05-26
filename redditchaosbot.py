a20900f97466f1607e25abf246dbebacf414f92b
Micah Nordland <micah@rehack.me>
2017-05-25 22:49:03 -0400

    changed bot to read from file

--- redditchaosbot.py
+++ redditchaosbot.py
@@ -1,20 +1,9 @@
 import logging
+import json
+import sys
 import time
 import praw
 
-
-'''Authenticated instance of Reddit'''
-# --------------------------------------------------------------------
-# https://praw.readthedocs.io/en/latest/getting_started/installation.html
-# https://www.reddit.com/prefs/apps
-reddit = praw.Reddit(
-    user_agent='Chaosbot social experiment',
-    client_id='WbDSRDEenyFRYw',
-    client_secret="CzAcNHiH91NM6baL1ypzQnkyFG8",
-    username='chaosthebotreborn',
-     password='9ws#?&+@3YBHLB~iVH,;5:rRr]@<,tW]J(n]t#`dZ;a7ZbsqJVny)(2yUO3d5d}VjW=L.U7aS4*E<-6x&WE$qa6>#{\Z\d]N-}T7')
-
-subreddit = reddit.subreddit('chaosthebot')
 log = logging.getLogger("chaosbot")
 handler = logging.StreamHandler()
 formatter = logging.Formatter(
@@ -22,6 +11,27 @@ formatter = logging.Formatter(
 handler.setFormatter(formatter)
 log.addHandler(handler)
 log.setLevel(logging.INFO)
+
+'''Authenticated instance of Reddit'''
+# --------------------------------------------------------------------
+# https://praw.readthedocs.io/en/latest/getting_started/installation.html
+# https://www.reddit.com/prefs/apps
+
+try:
+    with open("redditbot.config") as config_file:
+        config = json.load(config_file)
+except FileNotFoundError as ex:
+    log.critical(ex)
+    sys.exit(1)
+
+reddit = praw.Reddit(
+    user_agent='Chaosbot social experiment',
+    client_id=config['client_id'],
+    client_secret=config['client_secret'],
+    username='chaosthebotreborn',
+     password=config['password'])
+
+subreddit = reddit.subreddit('chaosthebot')
 
 
 already_replied = set()
@@ -35,6 +45,7 @@ except FileNotFoundError:
 def process_comment(comment):
     log.info("Processing comment id %s", comment.id)
     if comment.id not in already_replied and "hey chaosbot" in comment.body.lower():
+        log.info("Attempting to reply to comment %s", comment.id)
         comment.reply("Hey {}!".format(comment.author.name))
         already_replied.add(comment.id)
 
@@ -49,7 +60,7 @@ def main():
                 log.info("Sleeping for %d seconds", sleep_time)
                 time.sleep(sleep_time)
     finally:
-        with open("redditbot_replied.txt", 'a') as f:
+        with open("redditbot_replied.txt", 'w') as f:
             for id in already_replied:
                 f.write(id+"\n")
 
