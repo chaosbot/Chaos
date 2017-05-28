5a969bc0e2d2d44deec66285e921d31301d3554c
Phil <phil.ficus@gmail.com>
2017-05-28 17:20:34 +0200

    Check PR status `context` instead of `description` - added more tests

--- tests/github_api/prs_test.py
+++ tests/github_api/prs_test.py
@@ -6,7 +6,24 @@ from github_api import prs, API
 class TestPRMethods(unittest.TestCase):
     def test_statuses_returns_passed_travis_build(self):
         statuses = [{"state": "success",
-                    "description": "The Travis CI build passed"}]
+                    "context": "continuous-integration/travis-ci/pr"}]
+        pr = "/repos/test/blah"
+
+        class Mocked(API):
+            def __call__(m, method, path, **kwargs):
+                self.assertEqual(pr, path)
+                return statuses
+
+        api = Mocked("user", "pat")
+        url = "{}{}".format(api.BASE_URL, pr)
+
+        self.assertTrue(prs.has_build_passed(api, url))
+
+        # should succeed even if there are other statuses
+        statuses = [{"state": "success",
+                    "context": "continuous-integration/travis-ci/pr"},
+                    {"state": "failure",
+                    "context": "chaosbot"}]
         pr = "/repos/test/blah"
 
         class Mocked(API):
@@ -20,8 +37,10 @@ class TestPRMethods(unittest.TestCase):
         self.assertTrue(prs.has_build_passed(api, url))
 
     def test_statuses_returns_failed_travis_build(self):
+
+        # should fail because of "error" status
         statuses = [{"state": "error",
-                    "description": "The Travis CI build failed"}]
+                    "context": "continuous-integration/travis-ci/pr"}]
         pr = "/repos/test/blah"
 
         class Mocked(API):
@@ -34,8 +53,24 @@ class TestPRMethods(unittest.TestCase):
 
         self.assertFalse(prs.has_build_passed(api, url))
 
+        # should fail because of "pending" status
         statuses = [{"state": "pending",
-                    "description": "The Travis CI build is in progress"}]
+                    "context": "continuous-integration/travis-ci/pr"}]
+        pr = "/repos/test/blah"
+
+        class Mocked(API):
+            def __call__(m, method, path, **kwargs):
+                self.assertEqual(pr, path)
+                return statuses
+
+        api = Mocked("user", "pat")
+        url = "{}{}".format(api.BASE_URL, pr)
+
+        self.assertFalse(prs.has_build_passed(api, url))
+
+        # should fail because of incorrect context
+        statuses = [{"state": "pending",
+                    "context": "not-so-continuous-integration/travis-ci/pr"}]
         pr = "/repos/test/blah"
 
         class Mocked(API):
