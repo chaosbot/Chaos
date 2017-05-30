chaosbot/Chaos

commit 3e6d99c21af59a009c66aa525c03ac17c9b245dc

merging PR #377: Add a meritocracy
https://github.com/chaosbot/Chaos/pull/377: Add a meritocracy

Description:
I decided to not go with a clickbait title this time :smile:

Look at #361 for more details, but **I tried to give the meritocracy minimal power. Democracy still decides the outcome >90% of the time.** This is just to prevent against sneaky take over attempts.

Basis of how it works:

> At least one positive non-author meritocracy review on the most recent commit of a PR is required for that PR to be accepted.

Changes from #361:

- Autogenerate meritocracy list (currently top 10 contributors + top 10 voters)
- Don't count reviews as votes (so members of the meritocracy can allow a PR without voting for it)

:ok_woman: PR passed with a vote of 30 for and 1 against, with a weighted total             of 27.5 and a threshold of 6.5.

Vote record:
@CheezBallzPi: 1
@CodingPower472: 1
@DasSkelett: 1
@Evanito: 1
@FrankVanVuuren: 1
@Leigende: 1
@MUCHZER: 1
@N00D13: 1
@Neofox: 1
@PlasmaPower: 1
@Redmega: 1
@Smittyvb: 1
@SylvainThrd: 1
@andrewda: 1
@cthulhuely: 1
@davidadas: 1
@davidak: 1
@eukaryote31: 1
@flibustier: 1
@hongaar: 1
@ike709: 1
@jmerle: 1
@mark-i-m: 1
@md678685: 1
@mgardne8: -1
@phil-r: 1
@rhengles: 1
@rudehn: 1
@tarunbatra: 1
@viktorsec: 1
@vivekzhere: 1

cron/poll_pull_requests.py
@@ -23,99 +23,115 @@ def poll_pull_requests(api):
     # get all ready prs (disregarding of the voting window)
     prs = gh.prs.get_ready_prs(api, settings.URN, 0)
 
-    needs_update = False
-    for pr in prs:
-        pr_num = pr["number"]
-        __log.info("processing PR #%d", pr_num)
+    # This sets up a voting record, with each user having a count of votes
+    # that they have cast.
+    try:
+        fp = open('server/voters.json', 'x')
+        fp.close()
+    except:
+        # file already exists, which is what we want
+        pass
 
-        # gather all current votes
-        votes = gh.voting.get_votes(api, settings.URN, pr)
+    with open('server/voters.json', 'r+') as fp:
+        total_votes = {}
+        fs = fp.read()
+        if fs:
+            # if the voting record exists, read it in
+            total_votes = json.loads(fs)
+            # then prepare for overwriting
+            fp.seek(0)
+            fp.truncate()
 
-        # is our PR approved or rejected?
-        vote_total, variance = gh.voting.get_vote_sum(api, votes)
-        threshold = gh.voting.get_approval_threshold(api, settings.URN)
-        is_approved = vote_total >= threshold
+        top_contributors = sorted(gh.repos.get_contributors(api, settings.URN),
+                                  key=lambda user: user["total"], reverse=True)
+        top_contributors = top_contributors[:settings.MERITOCRACY_TOP_CONTRIBUTORS]
+        top_contributors = set([item["author"]["login"].lower() for item in top_contributors])
+        top_voters = sorted(total_votes, key=total_votes.get, reverse=True)
+        top_voters = set([user.lower() for user in top_voters[:settings.MERITOCRACY_TOP_VOTERS]])
+        meritocracy = top_voters | top_contributors
+        __log.info("generated meritocracy: " + str(meritocracy))
 
-        # the PR is mitigated or the threshold is not reached ?
-        if variance >= threshold or not is_approved:
-            voting_window = gh.voting.get_extended_voting_window(api, settings.URN)
+        needs_update = False
+        for pr in prs:
+            pr_num = pr["number"]
+            __log.info("processing PR #%d", pr_num)
 
-        # is our PR in voting window?
-        in_window = gh.prs.is_pr_in_voting_window(pr, voting_window)
+            # gather all current votes
+            votes, meritocracy_satisfied = gh.voting.get_votes(api, settings.URN, pr, meritocracy)
 
-        if is_approved:
-            __log.info("PR %d status: will be approved", pr_num)
+            # is our PR approved or rejected?
+            vote_total, variance = gh.voting.get_vote_sum(api, votes)
+            threshold = gh.voting.get_approval_threshold(api, settings.URN)
+            is_approved = vote_total >= threshold and meritocracy_satisfied
 
-            gh.prs.post_accepted_status(
-                api, settings.URN, pr, voting_window, votes, vote_total, threshold)
+            # the PR is mitigated or the threshold is not reached ?
+            if variance >= threshold or not is_approved:
+                voting_window = gh.voting.get_extended_voting_window(api, settings.URN)
 
-            if in_window:
-                __log.info("PR %d approved for merging!", pr_num)
+            # is our PR in voting window?
+            in_window = gh.prs.is_pr_in_voting_window(pr, voting_window)
 
-                try:
-                    sha = gh.prs.merge_pr(api, settings.URN, pr, votes, vote_total,
-                                          threshold)
-                # some error, like suddenly there's a merge conflict, or some
-                # new commits were introduced between finding this ready pr and
-                # merging it
-                except gh.exceptions.CouldntMerge:
-                    __log.info("couldn't merge PR %d for some reason, skipping",
-                               pr_num)
-                    gh.prs.label_pr(api, settings.URN, pr_num, ["can't merge"])
-                    continue
+            if is_approved:
+                __log.info("PR %d status: will be approved", pr_num)
 
-                gh.comments.leave_accept_comment(
-                    api, settings.URN, pr_num, sha, votes, vote_total, threshold)
-                gh.prs.label_pr(api, settings.URN, pr_num, ["accepted"])
+                gh.prs.post_accepted_status(
+                    api, settings.URN, pr, voting_window, votes, vote_total,
+                    threshold, meritocracy_satisfied)
 
-                # chaosbot rewards merge owners with a follow
-                pr_owner = pr["user"]["login"]
-                gh.users.follow_user(api, pr_owner)
+                if in_window:
+                    __log.info("PR %d approved for merging!", pr_num)
 
-                needs_update = True
+                    try:
+                        sha = gh.prs.merge_pr(api, settings.URN, pr, votes, vote_total,
+                                              threshold, meritocracy_satisfied)
+                    # some error, like suddenly there's a merge conflict, or some
+                    # new commits were introduced between finding this ready pr and
+                    # merging it
+                    except gh.exceptions.CouldntMerge:
+                        __log.info("couldn't merge PR %d for some reason, skipping",
+                                   pr_num)
+                        gh.prs.label_pr(api, settings.URN, pr_num, ["can't merge"])
+                        continue
 
-        else:
-            __log.info("PR %d status: will be rejected", pr_num)
+                    gh.comments.leave_accept_comment(
+                        api, settings.URN, pr_num, sha, votes, vote_total,
+                        threshold, meritocracy_satisfied)
+                    gh.prs.label_pr(api, settings.URN, pr_num, ["accepted"])
 
-            if in_window:
-                gh.prs.post_rejected_status(
-                    api, settings.URN, pr, voting_window, votes, vote_total, threshold)
-                __log.info("PR %d rejected, closing", pr_num)
-                gh.comments.leave_reject_comment(
-                    api, settings.URN, pr_num, votes, vote_total, threshold)
-                gh.prs.label_pr(api, settings.URN, pr_num, ["rejected"])
-                gh.prs.close_pr(api, settings.URN, pr)
-            elif vote_total < 0:
-                gh.prs.post_rejected_status(
-                    api, settings.URN, pr, voting_window, votes, vote_total, threshold)
+                    # chaosbot rewards merge owners with a follow
+                    pr_owner = pr["user"]["login"]
+                    gh.users.follow_user(api, pr_owner)
+
+                    needs_update = True
+
             else:
-                gh.prs.post_pending_status(
-                    api, settings.URN, pr, voting_window, votes, vote_total, threshold)
+                __log.info("PR %d status: will be rejected", pr_num)
 
-        # This sets up a voting record, with each user having a count of votes
-        # that they have cast.
-        try:
-            fp = open('server/voters.json', 'x')
-            fp.close()
-        except:
-            # file already exists, which is what we want
-            pass
-
-        with open('server/voters.json', 'r+') as fp:
-            old_votes = {}
-            fs = fp.read()
-            if fs:
-                # if the voting record exists, read it in
-                old_votes = json.loads(fs)
-                # then prepare for overwriting
-                fp.seek(0)
-                fp.truncate()
-            for user in votes:
-                if user in old_votes:
-                    old_votes[user] += 1
+                if in_window:
+                    gh.prs.post_rejected_status(
+                        api, settings.URN, pr, voting_window, votes, vote_total,
+                        threshold, meritocracy_satisfied)
+                    __log.info("PR %d rejected, closing", pr_num)
+                    gh.comments.leave_reject_comment(
+                        api, settings.URN, pr_num, votes, vote_total, threshold,
+                        meritocracy_satisfied)
+                    gh.prs.label_pr(api, settings.URN, pr_num, ["rejected"])
+                    gh.prs.close_pr(api, settings.URN, pr)
+                elif vote_total < 0:
+                    gh.prs.post_rejected_status(
+                        api, settings.URN, pr, voting_window, votes, vote_total,
+                        threshold, meritocracy_satisfied)
                 else:
-                    old_votes[user] = 1
-            json.dump(old_votes, fp)
+                    gh.prs.post_pending_status(
+                        api, settings.URN, pr, voting_window, votes, vote_total,
+                        threshold, meritocracy_satisfied)
+
+            for user in votes:
+                if user in total_votes:
+                    total_votes[user] += 1
+                else:
+                    total_votes[user] = 1
+            json.dump(total_votes, fp)
 
             # flush all buffers because we might restart, which could cause a crash
             os.fsync(fp)

github_api/comments.py
@@ -33,8 +33,8 @@ def get_reactions_for_comment(api, urn, comment_id):
         yield reaction
 
 
-def leave_reject_comment(api, urn, pr, votes, total, threshold):
-    votes_summary = prs.formatted_votes_summary(votes, total, threshold)
+def leave_reject_comment(api, urn, pr, votes, total, threshold, meritocracy_satisfied):
+    votes_summary = prs.formatted_votes_summary(votes, total, threshold, meritocracy_satisfied)
     body = """
 :no_good: PR rejected {summary}.
 
@@ -43,8 +43,8 @@ Open a new PR to restart voting.
     return leave_comment(api, urn, pr, body)
 
 
-def leave_accept_comment(api, urn, pr, sha, votes, total, threshold):
-    votes_summary = prs.formatted_votes_summary(votes, total, threshold)
+def leave_accept_comment(api, urn, pr, sha, votes, total, threshold, meritocracy_satisfied):
+    votes_summary = prs.formatted_votes_summary(votes, total, threshold, meritocracy_satisfied)
     body = """
 :ok_woman: PR passed {summary}.
 

github_api/prs.py
@@ -12,7 +12,7 @@ from . import voting
 TRAVIS_CI_CONTEXT = "continuous-integration/travis-ci"
 
 
-def merge_pr(api, urn, pr, votes, total, threshold):
+def merge_pr(api, urn, pr, votes, total, threshold, meritocracy_satisfied):
     """ merge a pull request, if possible, and use a nice detailed merge commit
     message """
 
@@ -26,7 +26,7 @@ def merge_pr(api, urn, pr, votes, total, threshold):
     if record:
         record = "Vote record:\n" + record
 
-    votes_summary = formatted_votes_summary(votes, total, threshold)
+    votes_summary = formatted_votes_summary(votes, total, threshold, meritocracy_satisfied)
 
     pr_url = "https://github.com/{urn}/pull/{pr}".format(urn=urn, pr=pr_num)
 
@@ -79,21 +79,28 @@ Description:
             raise
 
 
-def formatted_votes_summary(votes, total, threshold):
+def formatted_votes_summary(votes, total, threshold, meritocracy_satisfied):
     vfor = sum(v for v in votes.values() if v > 0)
     vagainst = abs(sum(v for v in votes.values() if v < 0))
+    meritocracy_str = "a" if meritocracy_satisfied else "**NO**"
 
-    return ("with a vote of {vfor} for and {vagainst} against, with a weighted total \
-            of {total:.1f} and a threshold of {threshold:.1f}"
-            .strip().format(vfor=vfor, vagainst=vagainst, total=total, threshold=threshold))
+    return """
+with a vote of {vfor} for and {vagainst} against, a weighted total of {total:.1f} \
+and a threshold of {threshold:.1f}, and {meritocracy} current meritocracy review
+    """.strip().format(vfor=vfor, vagainst=vagainst, total=total, threshold=threshold,
+                       meritocracy=meritocracy_str)
 
 
-def formatted_votes_short_summary(votes, total, threshold):
+def formatted_votes_short_summary(votes, total, threshold, meritocracy_satisfied):
     vfor = sum(v for v in votes.values() if v > 0)
     vagainst = abs(sum(v for v in votes.values() if v < 0))
+    meritocracy_str = "✓" if meritocracy_satisfied else "✗"
 
-    return "vote: {vfor}-{vagainst}, weighted total: {total:.1f}, threshold: {threshold:.1f}" \
-        .strip().format(vfor=vfor, vagainst=vagainst, total=total, threshold=threshold)
+    return """
+vote: {vfor}-{vagainst}, weighted total: {total:.1f}, threshold: {threshold:.1f}, \
+meritocracy: {meritocracy}
+    """.strip().format(vfor=vfor, vagainst=vagainst, total=total, threshold=threshold,
+                       meritocracy=meritocracy_str)
 
 
 def label_pr(api, urn, pr_num, labels):
@@ -260,34 +267,37 @@ def get_reactions_for_pr(api, urn, pr):
         yield reaction
 
 
-def post_accepted_status(api, urn, pr, voting_window, votes, total, threshold):
+def post_accepted_status(api, urn, pr, voting_window, votes, total, threshold,
+                         meritocracy_satisfied):
     sha = pr["head"]["sha"]
 
     remaining_seconds = voting_window_remaining_seconds(pr, voting_window)
     remaining_human = misc.seconds_to_human(remaining_seconds)
-    votes_summary = formatted_votes_short_summary(votes, total, threshold)
+    votes_summary = formatted_votes_short_summary(votes, total, threshold, meritocracy_satisfied)
 
     post_status(api, urn, sha, "success",
                 "remaining: {time}, {summary}".format(time=remaining_human, summary=votes_summary))
 
 
-def post_rejected_status(api, urn, pr, voting_window, votes, total, threshold):
+def post_rejected_status(api, urn, pr, voting_window, votes, total, threshold,
+                         meritocracy_satisfied):
     sha = pr["head"]["sha"]
 
     remaining_seconds = voting_window_remaining_seconds(pr, voting_window)
     remaining_human = misc.seconds_to_human(remaining_seconds)
-    votes_summary = formatted_votes_short_summary(votes, total, threshold)
+    votes_summary = formatted_votes_short_summary(votes, total, threshold, meritocracy_satisfied)
 
     post_status(api, urn, sha, "failure",
                 "remaining: {time}, {summary}".format(time=remaining_human, summary=votes_summary))
 
 
-def post_pending_status(api, urn, pr, voting_window, votes, total, threshold):
+def post_pending_status(api, urn, pr, voting_window, votes, total, threshold,
+                        meritocracy_satisfied):
     sha = pr["head"]["sha"]
 
     remaining_seconds = voting_window_remaining_seconds(pr, voting_window)
     remaining_human = misc.seconds_to_human(remaining_seconds)
-    votes_summary = formatted_votes_short_summary(votes, total, threshold)
+    votes_summary = formatted_votes_short_summary(votes, total, threshold, meritocracy_satisfied)
 
     post_status(api, urn, sha, "pending",
                 "remaining: {time}, {summary}".format(time=remaining_human, summary=votes_summary))

github_api/repos.py
@@ -30,3 +30,8 @@ def get_creation_date(api, urn):
     """ returns the creation date of the repo """
     data = api("get", get_path(urn))
     return arrow.get(data["created_at"])
+
+
+def get_contributors(api, urn):
+    """ returns the list of contributors to the repo """
+    return api("get", "/repos/{urn}/stats/contributors".format(urn=urn))

github_api/voting.py
@@ -10,7 +10,7 @@ from . import repos
 import settings
 
 
-def get_votes(api, urn, pr):
+def get_votes(api, urn, pr, meritocracy):
     """ return a mapping of username => -1 or 1 for the votes on the current
     state of a pr.  we consider comments and reactions, but only from users who
     are not the owner of the pr.  we also make sure that the voting
@@ -18,6 +18,7 @@ def get_votes(api, urn, pr):
     can't acquire approval votes, then change the pr """
 
     votes = {}
+    meritocracy_satisfied = False
     pr_owner = pr["user"]["login"]
     pr_num = pr["number"]
 
@@ -26,15 +27,17 @@ def get_votes(api, urn, pr):
         votes[voter] = vote
 
     # get all the pr-review-based votes
-    for vote_owner, vote in get_pr_review_votes(api, urn, pr_num):
-        if vote and vote_owner != pr_owner:
-            votes[vote_owner] = vote
+    for vote_owner, is_current, vote in get_pr_review_votes(api, urn, pr):
+        if (vote > 0 and is_current and vote_owner != pr_owner
+                and vote_owner.lower() in meritocracy):
+            meritocracy_satisfied = True
+            break
 
     # by virtue of creating the PR, the owner defaults to a vote of 1
     if votes.get(pr_owner) != -1:
         votes[pr_owner] = 1
 
-    return votes
+    return votes, meritocracy_satisfied
 
 
 def get_pr_comment_votes_all(api, urn, pr_num):
@@ -94,15 +97,16 @@ def get_comment_reaction_votes(api, urn, comment_id):
             yield reaction_owner, vote
 
 
-def get_pr_review_votes(api, urn, pr_num):
+def get_pr_review_votes(api, urn, pr):
     """ votes made through
     https://help.github.com/articles/about-pull-request-reviews/ """
-    for review in prs.get_pr_reviews(api, urn, pr_num):
+    for review in prs.get_pr_reviews(api, urn, pr["number"]):
         state = review["state"]
         if state in ("APPROVED", "DISMISSED"):
             user = review["user"]["login"]
+            is_current = review["commit_id"] == pr["head"]["sha"]
             vote = parse_review_for_vote(state)
-            yield user, vote
+            yield user, is_current, vote
 
 
 def get_vote_weight(api, username):

patch.py
@@ -30,3 +30,4 @@ api_memoize = partial(memoize, blacklist={"api"}, backend=json_backend(cache_dir
 decorate(github_api.voting.get_vote_weight, api_memoize("1d"))
 decorate(github_api.repos.get_num_watchers, api_memoize("10m"))
 decorate(github_api.prs.get_is_mergeable, api_memoize("2m"))
+decorate(github_api.repos.get_contributors, api_memoize("1d"))

settings.py
@@ -98,3 +98,9 @@ CHAOSBOT_STDERR_LOG = "/var/log/supervisor/chaos-stderr.log"
 # The threshold for how old an issue has to be without comments before we try to
 # auto-close it. i.e. if an issue goes this long without comments
 ISSUE_STALE_THRESHOLD = 60 * 60 * 24 * 3  # 3 days
+
+# The top n contributors will be allowed in the meritocracy
+MERITOCRACY_TOP_CONTRIBUTORS = 10
+
+# The top n voters will be allowed in the meritocracy
+MERITOCRACY_TOP_VOTERS = 10

commit 9f4af544c4363ad29f343f4bb9110d60ffd56cb0

Fix voters.json logic (broken by #377)

cron/poll_pull_requests.py
@@ -36,11 +36,7 @@ def poll_pull_requests(api):
         total_votes = {}
         fs = fp.read()
         if fs:
-            # if the voting record exists, read it in
             total_votes = json.loads(fs)
-            # then prepare for overwriting
-            fp.seek(0)
-            fp.truncate()
 
         top_contributors = sorted(gh.repos.get_contributors(api, settings.URN),
                                   key=lambda user: user["total"], reverse=True)
@@ -131,10 +127,15 @@ def poll_pull_requests(api):
                     total_votes[user] += 1
                 else:
                     total_votes[user] = 1
-            json.dump(total_votes, fp)
 
-            # flush all buffers because we might restart, which could cause a crash
-            os.fsync(fp)
+        if fs:
+            # prepare for overwriting
+            fp.seek(0)
+            fp.truncate()
+        json.dump(total_votes, fp)
+
+        # flush all buffers because we might restart, which could cause a crash
+        os.fsync(fp)
 
     # we approved a PR, restart
     if needs_update: