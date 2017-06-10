cad1fffbeeb3bd052d0ea32c71575a51c2253468
eamanu <emmanuelarias30@gmail.com>
2017-06-10 20:32:22 -0300

    Fix encryptation problems, regenerate twitter keys @PlasmaPower

--- twitter_api/misc.py
+++ twitter_api/misc.py
@@ -1,4 +1,4 @@
-# from encryption import decrypt
+from encryption import decrypt
 
 
 def GetKeys(twitter_keys_path):
@@ -6,21 +6,21 @@ def GetKeys(twitter_keys_path):
     consumer_secret = ''
     access_token = ''
     access_secret = ''
-    # PATH = 'twitter_keys.secret/'
+    PATH = 'twitter_keys/'
     l_files = ['consumer_key', 'consumer_secret', 'access_token', 'access_secret']
 
     for k in l_files:
-        # f = open(PATH + k + ".secret", 'rb')
-        # key = f.read()
+        f = open(PATH + k, 'rb')
+        key = f.read()
         if (k == 'consumer_key'):
-            consumer_key = '0Vj9kAlcViVv2QL4d6UqNWaVo'
+            consumer_key = decrypt(key)
         if (k == 'consumer_secret'):
-            consumer_secret = '9CVW0rJZGaGH19EfV9XT1skRwp7pj24iucmojuDIGMePi0Nny3'
+            consumer_secret = decrypt(key)
         if (k == 'access_token'):
-            access_token = '873520342207221760-wjSr5HdjoeYaZzVLyQs9OvDrIaXD5JA'
+            access_token = decrypt(key)
         if (k == 'access_secret'):
-            access_secret = 'CN8IGEoOUVvvPCLE5uIuWQWC902cNIqbtCAgI5DWbD6IK'
-        # f.close()
+            access_secret = decrypt(key)
+        f.close()
     """
     for k in keys:
         try:
