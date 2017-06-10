4c6caf6934fa19a168d12cdd2a0dd33d3a028cea
eamanu <emmanuelarias30@gmail.com>
2017-06-09 22:34:57 -0300

    Reopen #443 according to #364 fix some bugs add new twitter post

--- chaos.py
+++ chaos.py
@@ -11,6 +11,7 @@ import settings
 import schedule
 import cron
 import shutil
+import datetime
 
 # this import must happen before any github api stuff gets imported.  it sets
 # up caching on the api functions so we don't run out of api requests
@@ -78,13 +79,17 @@ def main():
     api_twitter = ta.API_TWITTER(settings.TWITTER_API_KEYS_FILE)
 
     log.info("checking if I crashed before...")
-    ta.Twitter.PostTwitter("checking if I crashed before...", api_twitter.GetApi())
+    ta.Twitter.PostTwitter(datetime.datetime.ctime(datetime.datetime.now()) + \
+                           " - checking if I crashed before...", \
+                           api_twitter.GetApi())
 
     # check if chaosbot is not on the tip of the master branch
     check_for_prev_crash(api, log)
 
     log.info("starting up and entering event loop")
-    ta.Twitter.PostTwitter("starting up and entering event loop", api_twitter.GetApi())
+    ta.Twitter.PostTwitter(datetime.datetime.ctime(datetime.datetime.now()) + \
+                           " - starting up and entering event loop", \
+                           api_twitter.GetApi())
 
     os.system("pkill uwsgi")
 
@@ -96,7 +101,7 @@ def main():
                       "--daemonize", "/root/workspace/Chaos/log/uwsgi.log"])
 
     # Schedule all cron jobs to be run
-    cron.schedule_jobs(api)
+    cron.schedule_jobs(api,api_twitter)
 
     log.info("Setting description to {desc}".format(desc=settings.REPO_DESCRIPTION))
     github_api.repos.set_desc(api, settings.URN, settings.REPO_DESCRIPTION)
--- twitter_api/misc.py
+++ twitter_api/misc.py
@@ -6,9 +6,26 @@ def GetKeys(twitter_keys_path):
     consumer_secret = ''
     access_token = ''
     access_secret = ''
-    fo = open(twitter_keys_path, 'rb')
-    keys = fo.readlines()
+    PATH = 'twitter_keys.secret/'
+    l_files = ['consumer_key', 'consumer_secret', 'access_token', 'access_secret']
 
+    for k in l_files:
+        f = open(PATH + k + ".secret", 'rb')
+        key = f.read()
+        if (k == 'consumer_key'):
+            consumer_key = decrypt(key)
+            # consumer_key = 'N5JrjZDqCtQ346nXF80BV20v9'
+        if (k == 'consumer_secret'):
+            consumer_secret = decrypt(key)
+            # consumer_secret = 'enEPaLQXPjYERBhPOxrWws4l6uFjC0YD7klwRRITR09z6bHmI2'
+        if (k == 'access_token'):
+            access_token = decrypt(key)
+            # access_token = '732737955962486786-V84lxJ35cep5hNOPaQ6WYBs40AvRTX4'
+        if (k == 'access_secret'):
+            access_secret = decrypt(key)
+            # access_secret = 'PKNttIB5rExAFgaqaFZP1OOl7yppsExltzNkNmTZmnPQ4'
+        f.close()
+    """
     for k in keys:
         try:
             values = k.split('\n')[0].split('=')[1].strip()
@@ -23,5 +40,7 @@ def GetKeys(twitter_keys_path):
         except IndexError:
             # Maybe there are a '\n' between keys
             continue
+
+    """
     return {'consumer_key': consumer_key, 'consumer_secret': consumer_secret,
             'access_token': access_token, 'access_secret': access_secret}
